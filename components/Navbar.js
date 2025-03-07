9d8cce7920420bdd8a7d0d76dfa0156391da8372
parthgandhi22 <parthgandhi625@gmail.com>
2025-03-08 01:35:57 +0530

    parth

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,30 +2,25 @@ import Link from 'next/link';
 
 const navbar = () => {
   return (
-    <div className="bg-aq-800 w-full h-17 flex items-center sticky">
-           <Link className='text-3xl font-bold !m-4' href="/">
+    <div className="bg-aq-800 w-full h-17 flex items-center sticky px-4 md:px-12 py-2">
+      <Link className='text-3xl font-bold m-4' href="/">
         LearniVerse
       </Link>
-      <div className='gap-10 flex center !ml-70 font-semibold '>
-            <Link className='hover:text-blue-700' href="/">
-                Cohorts
-            </Link>
-            <Link className='hover:text-blue-700' href="/">
-                Doubts
-            </Link>
-            <Link className='hover:text-blue-700' href="/">
-            WorkShops
-            </Link>
+      <div className='gap-10 flex items-center ml-auto font-semibold hidden md:flex'>
+        <Link className='hover:text-blue-700' href="/">
+          Cohorts
+        </Link>
+        <Link className='hover:text-blue-700' href="/">
+          Doubts
+        </Link>
+        <Link className='hover:text-blue-700' href="/">
+          WorkShops
+        </Link>
       </div>
-      <div className='!ml-60 gap-5 flex  items-center text-center'>
-      <button className='border-2 rounded-2xl !p-3 w-25 hover:bg-red-400 cursor-pointer'>Login</button>
-      <button className='border-2 rounded-2xl !p-3 w-25 hover:bg-green-400 hover:text-black cursor-pointer bg-black text-white'>SignUp</button>
+      <div className='ml-auto md:ml-60 gap-5 flex items-center text-center'>
+        <button className='border-2 rounded-2xl p-3 w-25 hover:bg-red-400 cursor-pointer'>Login</button>
+        <button className='border-2 rounded-2xl p-3 w-25 hover:bg-green-400 hover:text-black cursor-pointer bg-black text-white'>SignUp</button>
       </div>
-     
-     
-
-
-
     </div>
   )
 }
--- pages/postlogin/dashboard.js
+++ pages/postlogin/dashboard.js
@@ -1,17 +1,22 @@
 import Image from "next/image";
+import Navbar from "@/components/Navbar";
+import Learningcohorts from "@/components/Learningcohorts";
+import Upcomingsessions from "@/components/Upcomingsessions";
+import Mentoringsessions from "@/components/Mentoringsessions";
 
 const dashboard = () => {
   return (
-    <div className="bg-gray-200 p-12 flex gap-12 flex-col">
-      {/* import navigation bar */}
+    <div className="bg-gray-100 px-4 md:px-12 flex flex-col gap-12 py-2">
+      <Navbar />
+
       <div>
-        <div className="flex justify-between">
-          <div className="flex gap-3">
-            <Image src="/man.svg" width={20} height={20} className="rounded-b-4xl" />
-            <span className="text-2xl font-bold">Welcome back, Parth Gandhi</span>
+        <div className="flex flex-col md:flex-row justify-between">
+          <div className="flex gap-3 items-center">
+            <Image src="/man.svg" width={20} height={20} />
+            <span className="text-2xl md:text-3xl font-bold">Welcome back, Parth Gandhi</span>
           </div>
-          <div className="flex gap-2">
-            <span className="flex gap-2 bg-amber-100 p-1 rounded-2xl">
+          <div className="flex gap-2 mt-4 md:mt-0">
+            <span className="flex gap-2 bg-amber-100 p-1 rounded-2xl items-center">
               <Image src="/coins.svg" width={20} height={20} className="rounded-b-4xl" />
               <span>275 coins</span>
             </span>
@@ -19,28 +24,44 @@ const dashboard = () => {
           </div>
         </div>
       </div>
-      <div className="flex justify-between">
-        <div className="bg-white w-[30%] flex gap-5 p-3 rounded-2xl">
+
+      <div className="flex flex-col md:flex-row justify-between gap-4">
+        <div className="bg-white w-full md:w-[30%] flex gap-5 p-3 rounded-2xl">
           <Image src="/calendar.svg" width={20} height={20} />
           <div className="flex flex-col">
-            <span>Enrolled contents</span>
-            <span className="text-3xl">2</span>
+            <span>Enrolled Cohorts</span>
+            <span className="text-3xl font-semibold">2</span>
           </div>
         </div>
-        <div className="bg-white w-[30%] flex gap-5 p-3 rounded-2xl">
-          <Image src="/calendar.svg" width={20} height={20} />
+        <div className="bg-white w-full md:w-[30%] flex gap-5 p-3 rounded-2xl">
+          <Image src="/clock.svg" width={20} height={20} />
           <div className="flex flex-col">
-            <span>Active Sessions</span>
-            <span className="text-3xl">5</span>
+            <span>Hours Mentored</span>
+            <span className="text-3xl font-semibold">5</span>
           </div>
         </div>
-        <div className="bg-white w-[30%] flex gap-5 p-3 rounded-2xl">
-          <Image src="/calendar.svg" width={20} height={20} />
+        <div className="bg-white w-full md:w-[30%] flex gap-5 p-3 rounded-2xl">
+          <Image src="/card.svg" width={20} height={20} />
           <div className="flex flex-col">
-            <span>Completed Courses</span>
-            <span className="text-3xl">3</span>
+            <span>Coins Earned</span>
+            <span className="text-3xl font-semibold">3</span>
           </div>
         </div>
+      </div>
+
+      <div>
+        <span className="text-2xl">Your Learning Cohorts</span>
+        <Learningcohorts />
+      </div>
+
+      <div>
+        <span className="text-2xl">Your Upcoming Sessions</span>
+        <Upcomingsessions />
+      </div>
+
+      <div>
+        <span className="text-2xl">Your Mentoring Sessions</span>
+        <Mentoringsessions />
       </div>
     </div>
   );
