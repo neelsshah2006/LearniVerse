2eece533410714e49a1843c9b62d9ec95bf6bd00
neelsshah2006 <neelsshah2006@gmail.com>
2025-03-08 16:49:35 +0530

    Update socket.js

--- pages/socket.js
+++ pages/socket.js
@@ -1,10 +1,12 @@
 import Webrtc from "@/components/Webrtc";
 import React from "react";
+import Socket from "@/components/Socket";
 
 const socket = () => {
   return (
     <div>
       <Webrtc />
+      <Socket />
     </div>
   );
 };
