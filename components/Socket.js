eb025ab2d66b07983c26f4a381a498c66dd7dafd
neelsshah2006 <neelsshah2006@gmail.com>
2025-03-08 16:52:16 +0530

    Update Socket.js

--- components/Socket.js
+++ components/Socket.js
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import io from "socket.io-client";
 
-const socket = io("http://localhost:3000", { path: "/api/socket" });
+const socket = io("https://learni-verse-theta.vercel.app/socket", { path: "/api/socket" });
 
 export default function Home() {
   const [messages, setMessages] = useState([]);
